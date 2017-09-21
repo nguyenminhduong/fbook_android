c547eeb5cfb82c5f282d8fa43ff486dd4254e2e0
tri <tranminhtri911@gmail.com>
2017-09-21 14:51:12 +0700

    [Sprint 5] Feature mainpage user book detail

--- app/src/main/java/com/framgia/fbook/screen/userinbookdetail/UserInBookDetailPresenter.kt
+++ app/src/main/java/com/framgia/fbook/screen/userinbookdetail/UserInBookDetailPresenter.kt
@@ -1,12 +1,20 @@
 package com.framgia.fbook.screen.userinbookdetail;
 
+import com.framgia.fbook.data.source.BookRepository
+import com.framgia.fbook.utils.rx.BaseSchedulerProvider
+import io.reactivex.disposables.CompositeDisposable
+
 /**
  * Listens to user actions from the UI ({@link UserInBookDetailActivity}), retrieves the data and updates
  * the UI as required.
  */
-open class UserInBookDetailPresenter : UserInBookDetailContract.Presenter {
+open class UserInBookDetailPresenter(
+    private val mBookRepository: BookRepository) : UserInBookDetailContract.Presenter {
 
   private var mViewModel: UserInBookDetailContract.ViewModel? = null
+
+  private val mCompositeDisposable: CompositeDisposable by lazy { CompositeDisposable() }
+  private lateinit var mBaseSchedulerProvider: BaseSchedulerProvider
 
   override fun onStart() {}
 
@@ -14,5 +22,9 @@ open class UserInBookDetailPresenter : UserInBookDetailContract.Presenter {
 
   override fun setViewModel(viewModel: UserInBookDetailContract.ViewModel) {
     mViewModel = viewModel
+  }
+
+  fun setSchedulerProvider(baseSchedulerProvider: BaseSchedulerProvider) {
+    mBaseSchedulerProvider = baseSchedulerProvider
   }
 }
